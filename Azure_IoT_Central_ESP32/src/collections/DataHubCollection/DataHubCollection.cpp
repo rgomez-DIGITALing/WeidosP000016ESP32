6394a83b6f5ea686b5622966cd86c0d45f255a8a
OriolFalip <Oriol.Falip@weidmueller.com>
2023-11-09 16:56:27 +0100

    Check Weidos Data Hub existance before use.

--- Azure_IoT_Central_ESP32/src/collections/DataHubCollection/DataHubCollection.cpp
+++ Azure_IoT_Central_ESP32/src/collections/DataHubCollection/DataHubCollection.cpp
@@ -30,7 +30,7 @@ void DataHubCollectionClass::push(flowMeterManagerData_t data, int slot){
 }
 
 void DataHubCollectionClass::loop(){
-    weidosDataHub->loop();
+    if(weidosDataHub) weidosDataHub->loop();
     for(int i=0; i<MAX_DATA_HUBS_ALLOWED; i++){
         if(em1phDataHubPool[i]) em1phDataHubPool[i]->loop();
         if(em3phDataHubPool[i]) em3phDataHubPool[i]->loop();
