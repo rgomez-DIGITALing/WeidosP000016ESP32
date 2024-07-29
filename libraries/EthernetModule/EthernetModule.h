7083e1ba402af5285413d3edf680a5f460cbdd88
rgomez-DIGITALing <78080024+rgomez-DIGITALing@users.noreply.github.com>
2024-07-29 16:53:48 +0200

    Add files via upload

--- libraries/EthernetModule/EthernetModule.h
+++ libraries/EthernetModule/EthernetModule.h
@@ -1,29 +1,36 @@
-#pragma once
-
 #include <Arduino.h>
 #include <Ethernet.h>
 
 
+#define DHCP_CHECK_NONE         (0)
+#define DHCP_CHECK_RENEW_FAIL   (1)
+#define DHCP_CHECK_RENEW_OK     (2)
+#define DHCP_CHECK_REBIND_FAIL  (3)
+#define DHCP_CHECK_REBIND_OK    (4)
 
 
 class EthernetModuleClass{
     public:
         void init();
         void loop();
-        bool begin();
-        void linkStatusChange();
         bool isNetworkUp(){ return networkUp; }
-        void setLocalIp(IPAddress* localIp){ this->localIp = localIp; }
-        void setGateway(IPAddress* gateway){ this->gateway = gateway; }
-        void setSubmask(IPAddress* submask){ this->submask = submask; }
-        void setDns(IPAddress* dns){ this->dns = dns; }
-        bool ipSet(IPAddress* ip){ return ip != nullptr; }
+        void setLocalIp(IPAddress* localIp);
+        void setGateway(IPAddress* gateway);
+        void setSubmask(IPAddress* submask);
+        void setDns(IPAddress* dns);
+        
+        //bool ipSet(IPAddress ip){ return ip != nullptr; }
 
     private:
+        bool begin();
+        int maintain();
+        void linkStatusChange();
+        char* getLinkStatusName();
+        char* getMaintainResultName(int rc);
+
         uint8_t mac[6];
-        //uint8_t* mac4;
-        bool networkUp = false;
         int prevLinkStatus = Unknown;
+        bool networkUp = false;
         IPAddress* localIp = nullptr;
         IPAddress* gateway = nullptr;
         IPAddress* submask = nullptr;
